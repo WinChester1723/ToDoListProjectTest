69030de808be012ee664f0f655530f779f11c2da
Pasha Pashazade <qordon23@gmail.com>
2023-03-28 04:10:13 +0400

    add post method in controller

--- ToDoListProjectTest/Controllers/TaskController.cs
+++ ToDoListProjectTest/Controllers/TaskController.cs
@@ -1,13 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using ToDoListProjectTest.Domain.ViewModel;
 using ToDoListProjectTest.Models;
+using ToDoListProjectTest.Service.Interfaces;
 
 namespace ToDoListProjectTest.Controllers
 {
     public class TaskController : Controller
     {
+        private readonly ITaskService _taskService;
+
+        public TaskController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
         [HttpGet]
         public IActionResult Index() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateTaskViewModel model)
+        {
+            var response = await _taskService.Create(model);
+
+            if (response.StatusCode == Domain.Enum.StatusCode.OK)
+            {
+                return Ok(new { description = response.Description });
+            }
+            return BadRequest(new { description = response.Description });
+        }
 
     }
 }
